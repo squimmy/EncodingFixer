dc1b6d62ccfd2866e236a93ceb8adf0ac1877e0c
Timothy du Heaume <timothy.duheaume@gmail.com>
2014-06-18 23:56:32 +0800

    add a class for converting encodings

---
+++ EncodingFixer/EncodingConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace EncodingFixer
+{
+    sealed class EncodingConverter
+    {
+        public Encoding SourceEncoding { get; set; }
+        public Encoding TargetEncoding { get; set; }
+
+        public EncodingConverter(Encoding sourceEncoding, Encoding targetEncoding)
+        {
+            SourceEncoding = sourceEncoding;
+            TargetEncoding = targetEncoding;
+        }
+
+        public string Convert(string text)
+        {
+            var rawBytes = SourceEncoding.GetBytes(text);
+            var converted = Encoding.Convert(TargetEncoding, Encoding.UTF8, rawBytes);
+            return Encoding.UTF8.GetString(converted);
+        }
+    }
+}
