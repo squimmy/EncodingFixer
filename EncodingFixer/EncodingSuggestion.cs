e928215a671871e8d4f321e746c9255f03efd469
Timothy du Heaume <timothy.duheaume@gmail.com>
2014-06-20 02:05:09 +0800

    add ViewModel for the EncodingSuggestions control

---
+++ EncodingFixer/EncodingSuggestion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodingFixer
+{
+    sealed class EncodingSuggestion
+    {
+        public readonly string Source;
+        public readonly string Target;
+
+        public EncodingSuggestion(string source, string target)
+        {
+            Source = source;
+            Target = target;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} --> {1}", Source, Target);
+        }
+    }
+}
---
+++ EncodingFixer/EncodingSuggestionsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodingFixer
+{
+    sealed class EncodingSuggestionsViewModel : BindableBase
+    {
+        public ObservableCollection<EncodingSuggestion> Suggestions { get; set; }
+        public EncodingSuggestion SelectedSuggestion { get; set; }
+        public bool IsVisible { get { return Suggestions.Count > 0; } }
+
+        public EncodingSuggestionsViewModel()
+        {
+            Suggestions = new ObservableCollection<EncodingSuggestion>();
+            Suggestions.CollectionChanged += (s, e) => OnPropertyChanged(() => IsVisible);
+        }
+    }
+}
