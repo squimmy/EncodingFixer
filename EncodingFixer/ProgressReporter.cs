05418437f15d729357fc8f527f3a5c56053f52ee
Timothy du Heaume <timothy.duheaume@gmail.com>
2014-06-20 00:56:44 +0800

    add a class to report progress while doing work
    batches progress updates to minimise resource contention when reporting
    progress (in this case, the Dispatcher queue)

---
+++ EncodingFixer/ProgressReporter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EncodingFixer
+{
+    sealed class ProgressReporter
+    {
+        private readonly int batchSize;
+        public int currentProgress;
+        private readonly Action incrementProgress;
+
+        public ProgressReporter(int batchSize, Action incrementProgress)
+        {
+            this.batchSize = batchSize;
+            this.incrementProgress = incrementProgress;
+            this.currentProgress = 0;
+        }
+
+        public T DoWork<T>(Func<T> f)
+        {
+            var result = f();
+            Interlocked.Increment(ref currentProgress);
+            if (Interlocked.CompareExchange(ref currentProgress, 0, batchSize) == batchSize)
+                incrementProgress();
+            return result;
+        }
+    }
+}
