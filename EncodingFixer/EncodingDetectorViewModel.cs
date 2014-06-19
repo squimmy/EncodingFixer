7d9ed9f0a89a13bf9456e40f2d12d52a14bcb2fe
Timothy du Heaume <timothy.duheaume@gmail.com>
2014-06-19 22:42:02 +0800

    add EncodingDetectorViewModel

---
+++ EncodingFixer/EncodingDetectorViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Windows.Input;
+
+namespace EncodingFixer
+{
+    sealed class EncodingDetectorViewModel : BindableBase
+    {
+        public ICommand DetectEncoding { get; private set; }
+        public string TargetText { get; set; }
+        public bool IsDetectingEncoding { get; set; }
+        public bool IsEnabled { get { return !IsDetectingEncoding; } }
+        public int Progress { get; set; }
+        public int TargetProgress { get; set; }
+
+        public EncodingDetectorViewModel(Action detectEncoding)
+        {
+            DetectEncoding = new DelegateCommand(detectEncoding);
+            IsDetectingEncoding = false;
+        }
+    }
+}
